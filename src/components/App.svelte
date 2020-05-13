7fb5401044dc0761b92515abdc5f3d1bee1acf06
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-13 10:14:06 -0500

    first commit

---
+++ src/components/App.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<style>
+  :global(body) {
+    background-color: #676778;
+    color: white;
+  }
+  .main {
+    display: flex;
+    justify-content: center;
+  }
+</style>
+
+<div class="main">
+  <h1>Hello!</h1>
+</div>
